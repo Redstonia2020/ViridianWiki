c4e9f8db9b9d00eab9768483c2407b18aae97d66
Redstonia <redstonia1234@gmail.com>
2021-10-27 17:45:45 +1300

    groundbreaking technology on menus

--- docs/menu.js
+++ docs/menu.js
@@ -1,1 +1,8 @@
-﻿
+﻿var sidebar;
+var content;
+
+window.onload = function () {
+    sidebar = document.getElementById("menu");
+    content = sidebar.getElementsByTagName("iframe")[0].contentDocument;
+    sidebar.innerHTML = content.document.innerHTML;
+}
