58aba7d452353c41c9f76178c4d0469b9d2b9cb6
Redstonia <redstonia1234@gmail.com>
2021-11-01 17:14:50 +1300

    neeeeeeee

---
+++ docs/wikipage.js
@@ -0,0 +1,21 @@
+﻿var search = location.search;
+var pagename;
+var pageurl;
+
+window.onload = function () {
+    pageurl = search.substring(1);
+    pagename = pageurl.replace("_", " ");
+    document.title = pagename + " " + document.title;
+    document.getElementById("title").innerHTML = pagename;
+}
+
+function loadpage() {
+    loadfile()
+}
+
+async function loadfile() {
+    const response = await fetch("pages/" + pageurl + ".txt");
+    const data = await response.text();
+
+    return data;
+}
